a397442535e2153ab081e10372148e962e2aa7cc
adria <adria@DESKTOP-GS6RDFP>
2021-12-20 19:44:24 -0300

    Adicionar arquivos de projeto.

---
+++ adrianorecode/Migrations/20211219131553_teste.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace adrianorecode.Migrations
+{
+    public partial class teste : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pessoas");
+
+            migrationBuilder.CreateTable(
+                name: "clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_clientes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "clientes");
+
+            migrationBuilder.CreateTable(
+                name: "pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pessoas", x => x.Id);
+                });
+        }
+    }
+}
---
+++ adrianorecode/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace adrianorecode.Models
+{
+    public class Context: DbContext
+    {
+        public Context(DbContextOptions<Context> options):base(options) 
+        {
+
+        }
+
+       
+        public DbSet<Clientes> clientes { get; set; }
+    }
+}
